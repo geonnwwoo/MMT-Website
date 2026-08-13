02376e69a20b932e8cecbddf187e39626eb64de0
Xinhui Yu <main.xinhui.yu@gmail.com>
2026-08-12 20:33:01 -0700

    Fixed hover effects and added testing image.

--- src/routes/merch/+page.svelte
+++ src/routes/merch/+page.svelte
@@ -1,9 +1,21 @@
 <script>
     /** @type {import('./$types').PageData} */
     import PageHeader from "$lib/components/PageHeader.svelte";
-    import CardPanelBox from "$lib/components/CardPanelBox.svelte"
+    import PanelBox from "$lib/components/PanelBox.svelte"
 
     const products = [
+        {
+            title: "Test Hoodie",
+            image: "/merch/merchtestimg.webp",
+            alt: "image of hoodie",
+            sizes: ["S", "L", "XL"]
+        },
+        {
+            title: "Test Hoodie",
+            image: "/merch/merchtestimg.webp",
+            alt: "image of hoodie",
+            sizes: ["S", "L", "XL"]
+        },
         {
             title: "Test Hoodie",
             image: "/merch/merchtestimg.webp",
@@ -37,28 +49,30 @@
 
 <PageHeader
 	title="Merch"
-	description="Shop Mustang Math Merch!"
-	button_url=""
+	description="Shop Mustang Math Merch"
+	button_url="#products"
 	button_text="Shop Now"
 	id=""
 />
 
-<CardPanelBox>
-    <div class="product-grid">
-        {#each products as product}
-            <div class="product">
-                <h2 class="title">{product.title}</h2>
-                <img class="image" src={product.image} alt={product.alt} loading="lazy"/>
-                <div class="size-container">
-                    {#each product.sizes as size}
-                        <button class="size-button">{size}</button>
-                    {/each}
-                    <p class="size-quantity">999</p>
-                </div>
+
+<div class="product-grid" id="products">
+    {#each products as product}
+        <PanelBox borderRadius="12px" style="display: flex; flex-direction: column; opacity: 1;">
+            <h2 class="title">{product.title}</h2>
+            <div class="image-container">
+                <img class="image" src={product.image} alt={product.alt} loading="lazy"/> <!--product image-->
             </div>
-        {/each}
-    </div>
-</CardPanelBox>
+            <div class="size-container">
+                {#each product.sizes as size}
+                    <button class="size-button">{size}</button>
+                {/each}
+            <p class="size-quantity">999</p> <!--product quantity-->
+            </div>
+        </PanelBox>
+    {/each}
+</div>
+
 
 <style>
 
@@ -73,22 +87,29 @@ h2 {
     box-sizing: border-box;
     grid-template-columns: repeat(auto-fill, minmax(600px, 1fr));
     justify-content: center;
+    background-color: #ffffff;
+    box-shadow: 5px 10px 10px rgb(0, 0, 0, 0.1);
+    opacity: 0.8;
+    transition: 0.5s;
+    margin: "10px";
+    padding: "10px";
     gap: 10px;
 }
 
 
-.product{
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    border: 1px solid #e0e0e0;
-    border-radius: 12px;
-    padding: 1.2rem;
+.image-container {
+    overflow: hidden;
+    align-self: center;
 }
 
 .image {
     object-fit: cover;
-}   
+    border-radius: 5px;
+    transition: 0.3s;
+}
+.image:hover {
+    transform: scale(1.05);
+}
 
 .size-container {
     display: flex;
@@ -99,8 +120,13 @@ h2 {
     background-color: white;
     border: 1px solid #e0e0e0;
     border-radius: 12px;
-    padding: .25rem 1.5rem;
-    margin-right: .25rem;
+    padding: 1.5rem;
+    margin-right: .5rem;
+}
+
+.size-button:hover {
+    cursor: pointer;
+    border-color: #333;
 }
 
 .size-quantity {
