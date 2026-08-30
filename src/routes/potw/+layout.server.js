7a5cb0d0ca1808875ab95e58f59885aa51bdc7bd
Geonwoo Kim <124229577+geonnwwoo@users.noreply.github.com>
2026-08-30 11:37:22 -0700

    replaced maa problems with ones from compose

--- src/routes/potw/+layout.server.js
+++ src/routes/potw/+layout.server.js
@@ -12,7 +12,7 @@ export async function load() {
 
 
     // START TIME. MODIFY TO CHANGE POTW SELECTION instead of server/potw.json
-    const startDate = new Date('2026-08-11T08:00:00Z');
+    const startDate = new Date('2026-08-30T08:00:00Z');
     const weekIndex = Math.floor((now-startDate)/(1000*60*60*24*7));
     const problemIndex = weekIndex % potwData.length;
     const latestPotw = potwData[problemIndex];
--- src/routes/potw/archive/+page.server.js
+++ src/routes/potw/archive/+page.server.js
@@ -3,7 +3,7 @@ import potwData from '../server/potw.json';
 export async function load() {
     
     // START TIME. MODIFY TO CHANGE POTW SELECTION instead of server/potw.json
-    const startDate = new Date('2026-08-11T08:00:00Z');
+    const startDate = new Date('2026-08-30T08:00:00Z');
     const now = new Date();
     const weekIndex = Math.floor((now-startDate)/(1000*60*60*24*7));
     const problemIndex = weekIndex % potwData.length;
